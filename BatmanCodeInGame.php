b02e752b7b822822459e8bc75e4c2ee3661d1637
VJoss <verdelet.jossua@gmail.com>
2024-01-02 16:59:32 +0100

    test

---
+++ BatmanCodeInGame.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * Code In Game Shadows Of the Knight Ep1
+ **/
+
+// $W: width of the building.
+// $H: height of the building.
+fscanf(STDIN, "%d %d", $W, $H);
+// $N: maximum number of turns before game over.
+fscanf(STDIN, "%d", $N);
+fscanf(STDIN, "%d %d", $X0, $Y0);
+// game loop
+
+// Initialiser les variables pour stocker la direction précédente et la position actuelle de Batman
+
+$currentHeight = floor($Y0 / 2);
+$currentWidth = floor($X0 / 2);
+
+$lastPositionX = null;
+$lastPositionY = null;
+// Boucle de jeu
+while (TRUE) {
+    // Lire la direction de la bombe
+    fscanf(STDIN, "%s", $bombDir);
+    $lastPositionX = &$X0;
+    $lastPositionY = &$XY;
+    // Ajuster la position de Batman en fonction de la direction
+    switch ($bombDir) {
+        case "U":
+            $Y0 = floor(abs($H - $Y0)/2)*-1;
+            break;
+        case "UR":
+            $Y0 = floor(abs($H - $Y0)/2)*-1;
+            $X0 = floor(abs($W - $X0)/2);
+            break;
+        case "R":
+            $X0 = floor(abs($W - $X0)/2);
+            break;
+        case "DR":
+            $Y0 = floor(abs($H - $Y0)/2);
+            $X0 = floor(abs($W - $X0)/2);
+            break;
+        case "D":
+            $Y0 = floor(abs($H - $Y0)/2);
+            break;
+        case "DL":
+            $Y0 = floor(abs($H - $Y0)/2);
+            $X0 = floor(abs($W - $X0)/2)*-1;
+            break;
+        case "L":
+            $X0 = floor(abs($W - $X0)/2)*-1;
+            break;
+        case "UL":
+            $Y0 = floor(abs($H - $Y0)/2)*-1;
+            $X0 = floor(abs($W - $X0)/2)*-1;
+            break;
+    }
+
+
+    // Afficher la nouvelle position de Batman
+    echo("$currentWidth $currentHeight\n");
+}
---
+++ codeInGamePuzzleTemperatures.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+
+// $n: the number of temperatures to analyse
+fscanf(STDIN, "%d", $n);
+$inputs = explode(" ", fgets(STDIN));
+$closeToZero = null;
+for ($i = 0; $i < $n ; $i++)
+{
+    $t = intval($inputs[$i]); // a temperature expressed as an integer ranging from -273 to 5526
+    $difference = abs($t - 0);
+
+    if ($closeToZero === null || $difference < abs($closeToZero - 0) || ($difference === abs($closeToZero - 0) && $t>0)){
+            $closeToZero = $t;
+        }
+    }
+
+// Write an answer using echo(). DON'T FORGET THE TRAILING \n
+// To debug: error_log(var_export($var, true)); (equivalent to var_dump)
+
+if ($closeToZero !== null) {
+    echo("$closeToZero\n");
+} else {
+    echo("0\n");
+}
+?>
